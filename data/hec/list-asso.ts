97ebde471295f4496b968adde9cbf0c0c7cf3b5a
rgeron <raphaelgeron123@gmail.com>
2025-04-01 13:35:42 +0200

    bks

--- data/hec/list-asso.ts
+++ data/hec/list-asso.ts
@@ -2,7 +2,7 @@ export const associations = [
   "180 Degrees Consulting HEC Paris",
   "Assrock HEC",
   "Avenir Vietnam",
-  "Backstage",
+  "Backstage n'existe pas",
   "Bureau des Arts d’HEC Paris",
   "Bureau Des Élèves HEC Paris",
   "Bureau des Juristes",
